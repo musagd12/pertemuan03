38b6319fe553963d82e6ca42066c37a25b4d1e21
musa <musa47058@gmail.com>
2020-04-10 16:23:00 +0800

    Membuat String dari array char

--- src/string/OperasiString.java
+++ src/string/OperasiString.java
@@ -1,12 +1,20 @@
 package string;
 
+import static java.lang.System.*;
+
 public class OperasiString {
 
     public static void main(String[] args) {
         String  kota ="Yogyakarta";
 
 
-        System.out.println(kota);
+        out.println(kota);
+
+        char[] uniskaChar = {'U','N','I','S','K','A'};
+        String uniskaString = new String(uniskaChar);
+
+        out.println(uniskaString);
+
 
     }
 }
