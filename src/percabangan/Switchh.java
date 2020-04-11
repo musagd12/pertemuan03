00aacf46e359782350b058446ea84b37636ae300
musa <musa47058@gmail.com>
2020-04-11 13:19:32 +0800

    Percabangan Swith dengan Char

---
+++ src/percabangan/Switchh.java
@@ -0,0 +1,33 @@
+package percabangan;
+
+public class Switchh {
+    public static  void main(String[] args){
+        char nilai='D';
+
+        switch (nilai){
+            case 'A':
+                System.out.println("Mhs     :\"Terima kasih pak\"");
+                System.out.println("Dosen   :\"Selamat YA\"");
+                break;
+            case 'B':
+                System.out.println("Mhs     :\"Kenapa ulun kada dapat A pak\"");
+                System.out.println("Dosen   :\"!#$Q%^$%&^\"");
+                break;
+            case 'C':
+                System.out.println("Mhs     :\"Ulun turun tarus pak ai, tugas pul jua\"");
+                System.out.println("Dosen   :\"Tapi bisa lah menjawab ujian ??\"");
+                System.out.println("Mhs     :\"Nah amun ituu hehhehe\"");
+                break;
+            default:
+                System.out.println("Mhs     :\"Ulun turun pul pak ai, tugas pul jua\"");
+                System.out.println("Dosen   :\"Ya garang ??\"");
+                System.out.println("Dosen   :\"Cek Berkas\"");
+                System.out.println("Mhs     :\"Menceleng sidin\"");
+                System.out.println("Mhs     :\"kabuurrr...\"");
+                break;
+
+
+
+        }
+    }
+}
